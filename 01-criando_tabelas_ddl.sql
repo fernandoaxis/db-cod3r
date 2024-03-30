a9360b02a60966217a1ccbf1ae290c26cf85f29d
Fernando <159575977+fernandoaxis@users.noreply.github.com>
2024-03-30 19:07:44 -0300

    Update 01-criando_tabelas_ddl.sql

--- 01-criando_tabelas_ddl.sql
+++ 01-criando_tabelas_ddl.sql
@@ -1,7 +1,8 @@
 -- Criando a tabela estado!
+USE wm;
 
 create table estados(
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
     sigla VARCHAR(2) NOT NULL,
     região ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,
