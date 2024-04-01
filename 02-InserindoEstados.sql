ffa6963ce4c34d2342bb9743ff50452d2e5a2e2e
Fernando <159575977+fernandoaxis@users.noreply.github.com>
2024-03-31 22:23:18 -0300

    Add files via upload

---
+++ 02-InserindoEstados.sql
@@ -0,0 +1,9 @@
+use wm;
+
+insert into estados
+	(nome, sigla, região, populacao)
+values
+	('Alagoas','AL', 'Nordeste',3.38),
+	('Amapá','AP', 'Norte',0.8),
+    ('Amazonas', 'AM', 'Norte', 4.06);
+
